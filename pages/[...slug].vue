b66df4f2b2b541df75ec9cbadff8ed0ff2d0f425
Vivien <1494277+vlemoine@users.noreply.github.com>
2023-01-26 09:03:56 -0500

    Update [...slug].vue

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,12 +1,32 @@
 <template>
-  <main class="col-span-3 p-8">
-    <ContentDoc />
+  <main class="py-4 px-8">
+    <ContentDoc class="entry | sm:grid" />
   </main>
 </template>
 
 
-<style>
-h1 {
-  @apply text-xl;
+<style lang="scss">
+.entry {
+  grid-template-columns: minmax(33.3vw, 1fr) 1fr;
+  grid-template-areas: 
+            "a a"
+            "b c"
+            "b d"
+            "b e";
+  grid-column-gap: 2rem;
+  h1 {
+  @apply font-bold mb-4 text-2xl;
+  grid-area: a;
 }
+h2 {
+  @apply mb-2 text-xl;
+}
+p:has(img) {
+  grid-area: b;
+}
+ul {
+  @apply text-lg;
+}
+}
+
 </style>
