a8ccf2ec08b23d3b9e293f13335d9e594359e245
Vivien <1494277+vlemoine@users.noreply.github.com>
2023-01-25 22:20:49 -0500

    replace index

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="col-span-3 p-8">
+    <ContentList v-slot="{ list }">
+      <div v-for="article in list" :key="article._path">
+        {{ photo(article.body.children) }}
+        <h2>{{ article.title }}</h2>
+        <p>{{ article.description }}</p>
+      </div>
+    </ContentList>
+  </main>
+
+  
+</template>
+
+<script setup>
+const photo = (arr) => arr.filter(child => child.tag === 'p')[0]?.children[0]?.props;
+</script>
