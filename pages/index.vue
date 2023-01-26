414062e68fb7d5846519d9e3eada9d72c56a2055
Vivien <1494277+vlemoine@users.noreply.github.com>
2023-01-26 09:03:54 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
-  <main class="col-span-3 p-8 grid grid-cols-3 gap-4 auto-rows-max">
+  <main class="gallery | p-8 grid gap-4 auto-rows-max">
     <ContentList v-slot="{ list }">
-      <template v-for="article in list" :key="article._path">
+      <template v-for="(article, i) in list" :key="article._path">
         <template v-if="article.title !== 'About'">
           <NuxtLink
             :to="article._path"
             class="border border-pink-700 bg-pink-900 rounded overflow-hidden text-center"
+            :style="`order:${order(list, i)}`"
           >
             <img
               :src="photo(article.body.children).src"
@@ -22,4 +23,11 @@
 <script setup>
 const photo = (arr) =>
   arr.filter((child) => child.tag === "p")[0]?.children[0]?.props;
+const order = (arr, i) => arr.length - i;
 </script>
+
+<style>
+.gallery {
+  grid-template-columns: repeat(auto-fill,  minmax(16rem, 1fr));
+}
+</style>
