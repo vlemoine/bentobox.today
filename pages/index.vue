cdbf3a79c473223a0c5151e079e148e97926a853
Vivien <1494277+vlemoine@users.noreply.github.com>
2023-01-25 22:52:59 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,25 @@
 <template>
-  <main class="col-span-3 p-8">
+  <main class="col-span-3 p-8 grid grid-cols-3 gap-4 auto-rows-max">
     <ContentList v-slot="{ list }">
-      <div v-for="article in list" :key="article._path">
-        {{ photo(article.body.children) }}
-        <h2>{{ article.title }}</h2>
-        <p>{{ article.description }}</p>
-      </div>
+      <template v-for="article in list" :key="article._path">
+        <template v-if="article.title !== 'About'">
+          <NuxtLink
+            :to="article._path"
+            class="border border-pink-700 bg-pink-900 rounded overflow-hidden text-center"
+          >
+            <img
+              :src="photo(article.body.children).src"
+              :alt="photo(article.body.children).alt"
+            />
+            <h2 class="font-bold my-2">{{ article.title }}</h2>
+          </NuxtLink>
+        </template>
+      </template>
     </ContentList>
   </main>
-
-  
 </template>
 
 <script setup>
-const photo = (arr) => arr.filter(child => child.tag === 'p')[0]?.children[0]?.props;
+const photo = (arr) =>
+  arr.filter((child) => child.tag === "p")[0]?.children[0]?.props;
 </script>
