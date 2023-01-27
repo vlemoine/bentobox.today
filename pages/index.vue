e8117d5a9049a683f0d30acdf55977cefa0bd892
Vivien <1494277+vlemoine@users.noreply.github.com>
2023-01-26 21:50:32 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,24 +3,27 @@
     <h1 class="text-lg font-bold mb-4">Past Boxes</h1>
     <div class="gallery | grid gap-4 auto-rows-max">
       <ContentList v-slot="{ list }">
-      <template v-for="(article, i) in list" :key="article._path">
-        <template v-if="article.title !== 'About'">
-          <NuxtLink
-            :to="article._path"
-            class="border border-pink-700 bg-pink-900 rounded overflow-hidden text-center"
-            :style="`order:${order(list, i)}`"
-          >
-            <img
-              :src="photo(article.body.children).src"
-              :alt="photo(article.body.children).alt"
-            />
-            <h2 class="font-bold my-2">{{ article.title }}</h2>
-          </NuxtLink>
+        <template v-for="(article, i) in list" :key="article._path">
+          <template v-if="article.title !== 'About'">
+            <NuxtLink
+              :to="article._path"
+              class="border border-pink-700 bg-pink-900 rounded overflow-hidden text-center"
+              :style="`order:${order(list, i)}`"
+            >
+              <nuxt-picture
+                placeholder
+                width="360"
+                height="360"
+                sizes="xs:480px sm:500px md:380px lg:260px xl:320px"
+                :src="photo(article.body.children).src"
+                :alt="photo(article.body.children).alt"
+              />
+              <h2 class="font-bold my-2">{{ article.title }}</h2>
+            </NuxtLink>
+          </template>
         </template>
-      </template>
-    </ContentList>
+      </ContentList>
     </div>
-    
   </main>
 </template>
 
@@ -32,6 +35,6 @@ const order = (arr, i) => arr.length - i;
 
 <style lang="scss">
 .gallery {
-  grid-template-columns: repeat(auto-fill,  minmax(240px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
 }
 </style>
