1142e2497eb48bdf7fa1b4e1d43276e8caea2e25
Vivien <1494277+vlemoine@users.noreply.github.com>
2023-01-27 08:35:03 -0500

    add attributes

--- pages/index.vue
+++ pages/index.vue
@@ -3,35 +3,29 @@
     <h1 class="text-lg font-bold mb-4">Past Boxes</h1>
     <div class="gallery | grid gap-4 auto-rows-max">
       <ContentList v-slot="{ list }">
-      <template v-for="(article, i) in list" :key="article._path">
-        <template v-if="article.title !== 'About'">
-          <NuxtLink
-            :to="article._path"
-            class="border border-pink-700 bg-pink-900 rounded overflow-hidden text-center"
-            :style="`order:${order(list, i)}`"
-          >
-            <img
-              :src="photo(article.body.children).src"
-              :alt="photo(article.body.children).alt"
-            />
-            <h2 class="font-bold my-2">{{ article.title }}</h2>
-          </NuxtLink>
+        <template v-for="(article, i) in list" :key="article._path">
+          <template v-if="article.title !== 'About'">
+            <NuxtLink
+              :to="article._path"
+              class="border border-pink-700 bg-pink-900 rounded overflow-hidden text-center"
+              :style="`order:${order(list, i)}`"
+            >
+              <img :src="article.img" :alt="article.alt" />
+              <h2 class="font-bold my-2">{{ article.title }}</h2>
+            </NuxtLink>
+          </template>
         </template>
-      </template>
-    </ContentList>
+      </ContentList>
     </div>
-    
   </main>
 </template>
 
 <script setup>
-const photo = (arr) =>
-  arr.filter((child) => child.tag === "p")[0]?.children[0]?.props;
 const order = (arr, i) => arr.length - i;
 </script>
 
 <style lang="scss">
 .gallery {
-  grid-template-columns: repeat(auto-fill,  minmax(240px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
 }
 </style>
