66539ed25c14831bf1a2a6ecf2a176b7bb031f60
Vivien <1494277+vlemoine@users.noreply.github.com>
2023-10-11 22:14:21 -0400

    astro updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), mdx()]
-});
-
-export default defineConfig({
   site: 'https://vlemoine.github.io',
   base: '/bentobox.today',
-})
+  integrations: [tailwind(), mdx()]
+});
